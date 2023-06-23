daf7c4d4812851b86f010b3f6e8a8ffcccc4f363
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-23 16:08:16 +0300

    add path to apt-get

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,10 @@
 # Using Puppet, install flask from pip 3. Ensure Python 3.8.10 is installed, Flask 2.1.0 is installed.
+$bin = '/usr/bin'
 
 # Update the package manager
 exec { 'update':
   command => 'apt-get update',
+  path	=> $bin,
 }
 
 # Install Python 3.8.10 using apt
